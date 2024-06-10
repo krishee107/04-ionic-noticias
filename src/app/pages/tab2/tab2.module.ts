48f238544169ed6e20bd5e1cc6f211f3eb81d456
krishee107 <krishee107@gmail.com>
2024-06-10 23:25:21 +0200

    Movido tabs a pages + commit inicial

--- src/app/pages/tab2/tab2.module.ts
+++ src/app/pages/tab2/tab2.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
@@ -17,4 +17,4 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule { }
